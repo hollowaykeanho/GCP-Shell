48d816331f189bbe79726076f8a16574c9989a7d
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2021-01-28 19:51:17 +0800

    container-clusters-get-credentials.sh: updated for styling
    The script has some spacing issue. Hence, let's fix it.
    
    This patch updates container-clusters-get-credentials.sh for
    styling compliance.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- container-clusters-get-credentials.sh
+++ container-clusters-get-credentials.sh
@@ -3,8 +3,8 @@
 REGION="us-central1"
 ZONE="${REGION}-b"
 
-
 CLUSTER="my-cluster"
+
 
 # get Google container's clusters' credentials
 gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE"
