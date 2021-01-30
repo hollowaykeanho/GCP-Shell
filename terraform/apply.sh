52323137f39508fe0f51e4bae139f21d48944744
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2021-01-30 09:51:18 +0800

    root: added terraform notes
    While seeing it has no value but there must be a reason Google
    recommends it, I might as well document it since it is easily
    forgotten.
    
    This patch adds terraform notes into root repository.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ terraform/apply.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# 1. go into Terraform repository (already init)
+# 2. perform apply to the k8s cluster
+terraform apply
---
+++ terraform/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# 1. go into Terraform repository
+# 2. init Terraform
+terraform init
