f41e03c8a247f48e5b58795f8b5d7dfa94e41cb4
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2021-02-10 09:52:55 +0800

    compute-firewall-create.sh: merge allowHTTP into it
    Since the existing compute-firewall-create.sh is quite useless and
    the allowHTTP is the standard version, we should merge them for
    organizational purposes.
    
    This patch merges compute-firewall-rules-allowHTTP-create.sh into
    compute-firewall-create.sh.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- compute-firewall-create.sh
+++ compute-firewall-create.sh
@@ -20,14 +20,26 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-NAME="www-firewall-network-1b"
-TAG="network-1b-tag"
-ALLOW_RULES="tcp:80"
+NAME="app-allow-http"
+NETWORK="my-internal-app"
+DIRECTION="INGRESS"
+ACTION="ALLOW"
+RULES="tcp:80"
+SOURCE_RANGES="0.0.0.0/0"
+PRIORITY="1000"
+TARGET_TAGS="lb-backend"
+PROJECT="qwiklabs-gcp-02-2defea3466d7"  # optional argument
 
 
 
 
-# create firewall rule
+# create new firewall rule
 gcloud compute firewall-rules create "$NAME" \
-	--target-tags "$TAG" \
-	--allow "$ALLOW_RULES"
+	--network "$NETWORK" \
+	--direction "$DIRECTION" \
+	--action "$ACTION" \
+	--rules "$RULES" \
+	--source-ranges "$SOURCE_RANGES" \
+	--priority "$PRIORITY" \
+	--target-tags "$TARGET_TAGS" \
+	--project "$PROJECT"
