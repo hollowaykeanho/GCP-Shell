1f8e12969438383cc15533c820180849cf4747b0
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2021-04-05 18:17:01 +0800

    root: added kms notes
    Since there is lab introduces the kms feature, I best log all the
    notes here. Although rarely used, it's easy to get forgotten.
    
    This patch adds kms notes into root repository.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

---
+++ kms-encrypt.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright 2021 "Holloway" Chew, Kean Ho <hollowaykeanho@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+PLAINTEXT="my super secret thing"
+OUTPUT="1.encrypted"
+
+AUTH_TOKEN="$(gcloud auth application-default print-access-token)"
+PROJECT="$(gcloud config get-value project)"
+URL="https://cloudkms.googleapis.com/v1/projects/${PROJECT}/locations/global/keyRings/${KEYRING}/cryptoKeys/${KEY}"
+KEYRING="test"
+KEY="qwiklab"
+
+
+
+
+# encrypt data with kms
+curl --header "Authorization:Bearer $AUTH_TOKEN" \
+	--header "Content-Type: application/json" \
+	--data-binary @- \
+	"$URL:encrypt" > "$OUTPUT" << EOF
+{
+	"plaintext": "$PLAINTEXT"
+}
+EOF
+
+
+
+
+# unwrap from json and then save to the same filename
+jq .ciphertext -r "$OUTPUT" > "${OUTPUT}.tmp"
+mv "${OUTPUT}.tmp" "$OUTPUT"
